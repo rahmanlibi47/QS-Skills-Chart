8b4b9f18d3b919b3d699aaa6ebcf9a9a3f7b0393
Libin Rahman <rahmanlibi47@gmail.com>
2025-10-01 16:15:47 +0530

    fixing skill metrics

--- app/layout.jsx
+++ app/layout.jsx
@@ -2,13 +2,16 @@ import './globals.css'
 import Providers from '../components/Providers'
 
 export const metadata = {
-  title: 'My Skills App',
-  description: 'Track skills with charts',
+  title: 'QS Skill Metrics',
+  description: 'Track skills',
 }
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/qs_skills.png" type="image/png" />
+      </head>
       <body>
         <Providers>{children}</Providers>
       </body>
